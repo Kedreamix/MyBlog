f743b910afa90865b7638f91173160e28bcf0c3d
Kedreamix <1016617094@qq.com>
2024-02-02 21:15:37 +0800

    update

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
+@REM npm config set proxy http://your-proxy-server:your-proxy-port
+@REM  npm config set https-proxy http://your-proxy-server:your-proxy-port
 # npm config set registry https://registry.npm.taobao.org
 npm install gulp --global
 npm install gulp-clean-css gulp-html-minifier-terser gulp-htmlclean gulp-fontmin gulp-terser --save

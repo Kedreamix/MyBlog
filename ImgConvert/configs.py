dd1d146f907ed653490bf322640c4aa1c92b9f55
Kedreamix <kedreamix@gmail.com>
2024-03-05 13:07:07 +0800

    update

---
+++ ImgConvert/configs.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/30 14:08
+# @Author  : CYX
+# @Email   : im.cyx@foxmail.com
+# @File    : configs.py
+# @Software: PyCharm
+# @Project : PicConvert
+
+
+import json
+import os
+import sys
+
+path = os.path.join(os.path.split(os.path.abspath(sys.argv[0]))[0], "cookies.json")
+
+''' loading json file '''
+with open(path, 'r') as fp:
+    dicts = json.loads(fp.read())
+
+""" include 4 modes, choose 1~4 as default mode(s) """
+default_modes = dicts['default_modes']
+total_modes = dicts['total_modes']
+
+class Setting_config(object):
+    """
+        User can set your own parameters here
+    """
+
+    ''' config extensions which will be detected as pattern '''
+    ''' (Warning!! some websites cannot support all extensions below) '''
+    ext = ['png', 'bmp', 'jpg', 'jpeg', 'gif','webp']
+    pattern = ''.join(']\(?.*\.' + i + '\)|' + '="+.*\.' + i + '"|' for i in ext)[:-1]
+
+    # csdn img-bed config
+    csdn_cookies = dicts['csdn_cookies']
+    # bilibili img-bed config
+    bili_cookies = dicts['bili_cookies']
+    # jianshu img-bed config
+    jianshu_cookies = dicts['jianshu_cookies']
+    # bokeyuan img-bed config
+    bokeyuan_cookies = dicts['bokeyuan_cookies']
+    # zhihu img-bed config
+    zhihu_cookies = dicts['zhihu_cookies']
+    # zhihu convert status (src, watermark_src, original_src)
+    zhihu_mode = "original_src"
+
+
+class CSDN_config(Setting_config):
+    cookies = Setting_config.csdn_cookies
+
+    up_headers = {
+        'x-image-app': 'direct_blog',
+        'x-image-dir': 'direct'
+    }
+    up_url = "https://imgservice.csdn.net/direct/v1.0/image/upload?watermark=&type=blog&rtype=markdown"
+
+    boundary = '----WebKitFormBoundaryKzs0YGpR02NCBive'
+    path_headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+        "Content-Type": f"multipart/form-data; boundary={boundary}"
+    }
+    path_url = "https://csdn-img-blog.oss-cn-beijing.aliyuncs.com/"
+
+    fields = {'art_id': 'undefined'}
+    cookies['UserInfo'] = cookies['UserToken']
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+        "content-type": "application/json"
+    }
+    convert_url = "https://imgservice.csdn.net/img-convert/external/storage"
+
+class ZHIHU_config(Setting_config):
+    cookies = Setting_config.zhihu_cookies
+    mode = Setting_config.zhihu_mode
+
+    boundary = '----WebKitFormBoundaryA7rHHPjs3umheW1O'
+    headers = {
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
+        "referer": "https://zhuanlan.zhihu.com/write",
+        "origin": "https://zhuanlan.zhihu.com",
+        "x-requested-with": "fetch",
+        "Content-Type": f"multipart/form-data; boundary={boundary}"
+    }
+    mode_dict = ['src', 'watermark_src', 'original_src']
+    fields = {'source': 'article'}
+    convert_url = "https://zhuanlan.zhihu.com/api/uploaded_images"
+
+class BILI_config(Setting_config):
+    cookies = Setting_config.bili_cookies
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
+        "referer": "https://member.bilibili.com/",
+        "origin": "https://member.bilibili.com",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    }
+    b64_head = 'data:image;base64,'
+    fields = {'csrf': cookies['bili_jct']}
+    convert_url = "https://api.bilibili.com/x/article/creative/article/upcover"
+
+class JIANSHU_config(Setting_config):
+    cookies = Setting_config.jianshu_cookies
+
+    boundary = '------WebKitFormBoundaryoDKCqoQaipUHiBdS'
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+        "Host": "upload.qiniup.com",
+        "Origin": "https://www.jianshu.com",
+        "Referer": "https://www.jianshu.com/",
+        "content-type": f"multipart/form-data; boundary={boundary}",
+        "Sec-Fetch-Dest": "empty"
+    }
+    token_url = "https://www.jianshu.com/upload_images/token.json?filename=image."
+    convert_url = "https://upload.qiniup.com/"
+
+class BOKEYUAN_config(Setting_config):
+    cookies = Setting_config.bokeyuan_cookies
+
+    boundary = '----WebKitFormBoundary0YtEgqslRTAf1lEB'
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
+        "origin": "https://i.cnblogs.com",
+        "referer": "https://i.cnblogs.com/",
+        "content-type": f"multipart/form-data; boundary={boundary}",
+        'x-xsrf-token': 'CfDJ8AuMt_3FvyxIgNOR82PHE4mor62BfuI58rIEHB4fHCfGnPXDShqSPjlPtYED-W3kcbAFy0VuhQU0xFNDVgbXQb_603YRMn_Mp9RbW9NkTV9pxpK8Yz3FQ0Oh9fw5jTMrmQZUZICAjUyRZVBgJ9H9zfpYW-QlLgVF6iJeJfR7ojwvT8QyU536rfXsAPFTUbL_xQ',
+    }
+    fields = {'host': 'www.cnblogs.com', 'uploadType': 'Paste'}
+    convert_url = "https://upload.cnblogs.com/imageuploader/CorsUpload/"
---
+++ ImgConvert/convert.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/14 18:31
+# @Author  : CYX
+# @Email   : im.cyx@foxmail.com
+# @File    : convert.py
+# @Software: PyCharm
+# @Project : PicConvert
+
+import argparse
+import os
+
+from src import img_convert
+from configs import default_modes, total_modes
+
+def get_name_list(path):
+    """
+    scan all root directory and children directory to find all markdown file
+    :return: all markdown name files
+    """
+    name_list = []
+    for files in os.listdir(path):
+        __, ext = os.path.splitext(files)
+        if ext == '.md':
+            name_list.append(files)
+    return name_list
+
+def handler(file, mode, link=False):
+    if link:
+        res = []
+        for f in file:
+            print(f)
+            res.append(img_convert(mode, f, os.path.dirname(f), link=True))
+        print("Upload Success:")
+        print('\n'.join(res))
+    else:
+        print(f"Reading File: {file}")
+        print("-"*50)
+        with open(file, "rb") as fp:
+            res = img_convert(mode, fp.read().decode("utf-8"), os.path.dirname(file))
+
+        new_file = f'{os.path.basename(file)}'
+        print("-"*50)
+        print(f"Writing File: {new_file}\n")
+        with open(new_file, "wb+") as fp2:
+            fp2.write(res.encode("utf-8"))
+
+def main():
+    parser = argparse.ArgumentParser(description='This script for you to store/convert pictures between pictures bed')
+    parser.add_argument('-f', default='', dest='file', type=str,
+                        help='set convert markdown file (optional, default select all markdown files in root directory)')
+    parser.add_argument('-p', default='./', dest='path', type=str)
+    parser.add_argument('-m', default='', dest='mode', type=str,
+                        help='set convert mode: zhihu/csdn/bili/jianshu (optional, default set in configs.py)')
+    parser.add_argument('-d', default='', dest='direct', type=str, nargs='+',
+                        help='directly convert')
+    args = parser.parse_args()
+    print(args)
+    # judge convert which type pic bed
+    if args.mode != '':
+        if args.mode in total_modes:
+            modes = [args.mode]
+        else:
+            raise NameError("Pls enter a correct mode!!")
+    else:
+        modes = default_modes
+
+    # if direct convert links/img_files
+    if args.direct:
+        name_list = args.direct
+        for mode in modes:
+            handler(name_list, mode, link=True)
+
+    # if convert files
+    else:
+        # judge which file convert
+        if args.file != '':
+            if os.path.exists(args.file):
+                name_list = [args.file]
+            else:
+                raise NameError("Pls enter a correct file name!")
+        else:
+            name_list = get_name_list(args.path)
+            if not name_list:
+                raise Exception('Markdown files not found!')
+
+        print(f"\nFiles: {'  '.join(name_list)}", f"\nModes: {'  '.join(modes)}", '\n')
+
+        # handle every markdown files or every links/img_files
+        for file in name_list:
+            file = os.path.join(args.path, file)
+            for mode in modes:
+                handler(file, mode)
+
+if __name__ == '__main__':
+    main()
